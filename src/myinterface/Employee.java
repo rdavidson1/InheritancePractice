87029d50c0670a3fbbb9b76bc13f268115961f2e
Ryan <Ryan@Ryan-PC.wi.rr.com>
2014-02-03 16:32:13 -0600

    Progress on Abstract and Interface pt2

---
+++ src/myinterface/Employee.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package myinterface;
+
+/**
+ *
+ * @author Ryan
+ */
+public abstract class Employee {
+    
+    
+    public void setEmployeeFName(){
+    
+    }
+    public void setEmployeeLName(){
+    
+    }
+    public void setEmployeeID(){
+    
+    }
+
+}
